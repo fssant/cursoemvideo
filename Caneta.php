cddc6510f7081cce94a06a3367e5f769632af2a2
Filipe Santos <fssantdev@gmail.com>
2018-09-29 13:00:20 -0300

    Add setters e getters

--- Caneta.php
+++ Caneta.php
@@ -6,6 +6,37 @@ class Caneta{
     protected $carga;
     protected $tampada;
 
+    public function setModelo($modelo){
+      $this->modelo = $modelo;
+    }
+    public function getModelo(){
+      return($this->modelo);
+    }
+    public function setCor($cor){
+      $this->cor = $cor;
+    }
+    public function getCor(){
+      return($this->cor);
+    }
+    public function setPonta($ponta){
+      $this->ponta = $ponta;
+    }
+    public function getPonta(){
+      return($this->ponta);
+    }
+    public function setCarga($carga){
+      $this->carga = $carga;
+    }
+    public function getCarga(){
+      return($this->carga);
+    }
+    public function setTampada($tampada){
+      $this->tampada = $tampada;
+    }
+    public function getTampada(){
+      return($this->tampada);
+    }
+    
     public function rabiscar(){
       $resposta = $this->tampada?("Caneta tampada,impossivel rabiscar"):("Rabiscando");
       echo($resposta);
